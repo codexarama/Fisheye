c82744d9fe054801fc1646dd7b4983f26b01ab9d
Amandine Arias <amandine.arias@gmail.com>
2021-05-03 20:16:52 +0200

    refactor (modal) : lightbox styles (#4)

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,34 +1,16 @@
+// importer la constante "medias" from "gallery.js"
+// import medias from "./gallery"
+
 const lightbox = document.querySelector('.lightbox__modal');
 
 // launch lightbox
 openLightbox = () => {
-  lightbox.style.display = 'block';
+  lightbox.style.display = 'flex';
 };
 
 // close lightbox
 closeLightbox = () => {
   lightbox.style.display = 'none';
+  lightboxShow.classList.remove('active');
 };
 
-// ---------- ne fonctionne pas --------- // A REVOIR
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// next - previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function showSlides(n) {
-  let slides = document.getElementsByClassName('lightbox__media');
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slides[slideIndex - 1].style.display = 'flex';
-}

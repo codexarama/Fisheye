c3bd4d25ae00bca31664af761bb95aa4f90f9215
Amandine Arias <amandine.arias@gmail.com>
2021-05-11 11:58:41 +0200

    fix (modal) : lightbox prev-next (#4)

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,3 +1,4 @@
+// DOM ELEMENTS
 const lightbox = document.querySelector('.lightbox__modal');
 const lightboxShow = document.querySelector('.lightbox__show');
 const lightboxMedia = document.querySelector('.lightbox__media');
@@ -8,7 +9,8 @@ const next = document.querySelector('.lightbox__next');
 // LAUNCH LIGHTBOX
 const setLightbox = () => {
   const medias = document.querySelectorAll('.currentMedia');
-  console.log(medias);
+  // console.log(medias);
+
   for (let i = 0; i < medias.length; i++) {
     let selectMedia = medias[i];
     let selectedMediaIndex = i;
@@ -16,34 +18,32 @@ const setLightbox = () => {
     // quand click sur image
     selectMedia.addEventListener('click', (e) => {
       e.preventDefault();
-      // let selectedMediaIndex = i;
-      console.log(selectMedia); // infos media choisi
-      console.log(i); // index media choisi
+      selectMedia.classList.add('selected');
+      // console.log(selectedMediaIndex); // index media choisi
+      // console.log(selectMedia); // infos media choisi
 
-      // OUVRE LIGHTBOX
-      // ---------- TEST FONCTION + APPELS ---------- //
-      // ---------- NE FONCTIONNE PAS ---------- //
-      // const openLightbox = () => {
+      // OUVRE LA LIGHTBOX
       lightbox.style.display = 'flex';
       lightboxShow.classList.add('active');
-      selectMedia.classList.add('selected');
-      // affiche titre media dans url
-      window.location.hash = selectMedia.alt;
-      // affiche media + titre dans lightbox
-      lightboxMedia.src = selectMedia.src;
-      lightboxMedia.alt = selectMedia.alt;
-      lightboxTitle.textContent = selectMedia.alt;
-      // };
+
+      // AFFICHE LE MEDIA CHOISI
+      showMedia();
 
       // quand click sur prev
       prev.addEventListener('click', (e) => {
         e.preventDefault();
-        // AFFICHE MEDIA PRECEDENT
+        selectMedia.classList.remove('selected');
+
         // index -1
         selectedMediaIndex--;
-        console.log(selectedMediaIndex);
-        console.log(selectMedia); // LA SOURCE N'EST PAS REMPLACEE PAR LE NOUVEL INDEX
-        // openLightbox();
+        selectMedia = medias[selectedMediaIndex];
+        selectMedia.classList.add('selected');
+        // console.log(selectedMediaIndex); // index media precedent
+        // console.log(selectMedia); // infos media precedent
+
+        // AFFICHE LE MEDIA PRECEDENT
+        showMedia();
+
         // gestion visibilite prev + next btn pour 1e et derniere image
         if (selectedMediaIndex == 0) prev.style.display = 'none';
         if (selectedMediaIndex < medias.length) next.style.display = 'block';
@@ -52,36 +52,36 @@ const setLightbox = () => {
       // quand click sur next
       next.addEventListener('click', (e) => {
         e.preventDefault();
-        // AFFICHE MEDIA SUIVANT
+        selectMedia.classList.remove('selected');
+
         // index +1
         selectedMediaIndex++;
-        console.log(selectedMediaIndex);
-        console.log(selectMedia); // LA SOURCE N'EST PAS REMPLACEE PAR LE NOUVEL INDEX
+        selectMedia = medias[selectedMediaIndex];
+        selectMedia.classList.add('selected');
+        // console.log(selectedMediaIndex); // index media suivant
+        // console.log(selectMedia); // infos media suivant
+
+        // AFFICHE LE MEDIA SUIVANT
+        showMedia();
+
         // gestion visibilite prev + next btn pour 1e et derniere image
         if (selectedMediaIndex == medias.length - 1)
           next.style.display = 'none';
         if (selectedMediaIndex > 0) prev.style.display = 'block';
       });
-      // openLightbox()
     });
+
+    // AFFICHE LE MEDIA CHOISI DANS LA LIGHTBOX
+    const showMedia = () => {
+      // affiche titre media dans url
+      window.location.hash = selectMedia.alt;
+      // affiche media + titre dans lightbox
+      lightboxMedia.src = selectMedia.src;
+      lightboxMedia.alt = selectMedia.alt;
+      lightboxTitle.textContent = selectMedia.alt;
+    };
   }
 };
-
-// // AUTRE METHODE (BY IMG.URL)
-// // ouvre l'image dans une autre pag web
-// const setLightbox = () => {
-//   for (let i = 0; i < medias.length; i++) {
-//     medias[i].onclick = () => {
-//       console.log(i);
-//       const showMedia = () => {
-//         // window.location.replace(medias[i].src)
-//         let selectedMedia = window.location.replace(medias[i].src)
-//         console.log(selectedMedia);
-//       };
-//       showMedia();
-//       lightbox.style.display = 'flex';
-//       lightboxShow.classList.add('active');
-//     };
 
 // CLOSE LIGHTBOX
 const closeLightbox = () => {

9c6675f2a1387f6d9049140d4fd1f226d30552e7
Amandine Arias <amandine.arias@gmail.com>
2021-05-12 14:21:47 +0200

    manage (accessibility filter) : improvements (#6)

--- js/filtres.js
+++ js/filtres.js
@@ -1,40 +1,67 @@
-// AFFICHE NOM OPTION PAR DEFAUT
-document.querySelector('.filter__selected').setAttribute('value', 'Popularité');
-
-// IDENTITFIE OPTION CHOISIE
-// ouvre la box au click
+// OUVRE - FERME LISTBOX
 document.querySelector('.filter').addEventListener('click', function () {
   this.querySelector('.filter__box').classList.toggle('open');
 });
 
-// itere sur les options
-for (const option of document.querySelectorAll('.filter__option')) {
-  option.addEventListener('click', function () {
-    if (!option.classList.contains('selected')) {
-      option.parentNode
-      // retire "selected" sur ancienne option
-        .querySelector('.filter__option.selected')
-        .classList.remove('selected');
-      // affecte "selected" sur nouvelle option
-      option.classList.add('selected');
-      option
-        .closest('.filter__box')
-        .querySelector('.filter__selected')
-        // affecte classe "button" sur input
-        .classList.add('btn');
-      option
-        .closest('.filter__box')
-        .querySelector('.filter__selected')
-        // affecte nom option choisie dans input
-        .setAttribute('value', this.textContent);
-    }
-  });
+const listbox = document.querySelector('[role="listbox"]');
+// console.log(listbox);
+const options = [...listbox.children];
+// console.log(options);
 
-  // ferme listbox si click hors box
-  window.addEventListener('click', function (e) {
-    const select = document.querySelector('.filter__box');
-    if (!select.contains(e.target)) {
-      select.classList.remove('open');
-    }
-  });
-}
+// IDENTITFIE OPTION CHOISIE
+// navigation souris
+listbox.addEventListener('click', (event) => {
+  const selectedOption = event.target.closest('li');
+  if (!selectedOption) return;
+
+  // ACCESSIBILITE
+  // attribue valeur aria-activedescendant
+  listbox.setAttribute('aria-activedescendant', selectedOption.id);
+
+  // retire "selected" sur ancienne option
+  // affecte "selected" sur nouvelle option
+  options.forEach((option) => option.classList.remove('selected'));
+  selectedOption.classList.add('selected');
+  // remplace texte bouton par nom option choisie
+  selectedOption
+    .closest('.filter__box')
+    .querySelector('.filter__selected').textContent =
+    selectedOption.textContent;
+});
+
+// FERME LISTBOX SI CLICK HORS BOX
+window.addEventListener('click', function (e) {
+  const select = document.querySelector('.filter__box');
+  if (!select.contains(e.target)) {
+    select.classList.remove('open');
+  }
+});
+
+// NAVIGATION CLAVIER
+let listboxBtn = document.querySelector('#orderBy');
+
+// listboxBtn = listbox;
+// console.log(listboxBtn);
+
+listbox.addEventListener('keydown', (event) => {
+  const { key } = event;
+  if (key !== 'ArrowDown') return;
+
+  listbox.setAttribute('aria-activedescendant', selectedOption.id);
+  const activeElementID = listbox.getAttribute('aria-activedescendant');
+  console.log(activeElementID);
+  const activeElement = listbox.querySelector('#' + activeElementID);
+  console.log(activeElement);
+  // const selectedOption = activeElement.nextElementSibling;
+  // const nextElement = activeElement.nextElementSibling;
+
+  // if (nextElement) {
+  //   // attribue valeur aria-activedescendant
+  //   listbox.setAttribute('aria-activedescendant', selectedOption.id);
+
+  //   // retire "selected" sur ancienne option
+  //   // affecte "selected" sur nouvelle option
+  //   options.forEach((option) => option.classList.remove('selected'));
+  //   selectedOption.classList.add('selected');
+  // }
+});

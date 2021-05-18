023359dcf5c9f75a9eb0210f3265195f85748627
Amandine Arias <amandine.arias@gmail.com>
2021-05-18 21:17:15 +0200

    feat (modal) : lightbox > show video (#4)

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,10 +1,13 @@
 // DOM ELEMENTS
 const lightbox = document.querySelector('.lightbox__modal');
-const lightboxShow = document.querySelector('.lightbox__show');
-const lightboxMedia = document.querySelector('.lightbox__media');
-const lightboxTitle = document.querySelector('.lightbox__title');
+let lightboxShow = document.querySelector('.lightbox__show');
+// const lightboxTitle = document.querySelector('.lightbox__title');
 const prev = document.querySelector('.lightbox__prev');
 const next = document.querySelector('.lightbox__next');
+
+// const typeImage = document.querySelectorAll('img')
+// const typeImage = document.querySelector('.currentMedia img')
+// console.log(typeImage);
 
 const setLightbox = () => {
   const medias = document.querySelectorAll('.gallery__link');
@@ -23,13 +26,20 @@ const setLightbox = () => {
     // quand click sur media
     selectMedia.addEventListener('click', (event) => {
       event.preventDefault();
-
       // OUVRE LA LIGHTBOX
       lightboxShow.classList.add('active');
       lightbox.style.display = 'flex';
 
       // affecte 'selected' au media choisi
       selectedMedia.classList.add('selected');
+
+      // // RESET LA LIGHTBOX
+      // // ----------- NE FONCTIONNE PAS ---------- //
+      // const lightboxMedia = document.querySelectorAll('lightbox__media');
+      // for (let i = 0; i < lightboxMedia.length; i++) {
+      //   if (lightboxMedia.length > 0) lightboxShow.removeChild(img);
+      //   // if (lightboxMedia.length > 0) lightboxShow.removeChild(lightboxMedia);
+      // }
 
       // AFFICHE LE MEDIA CHOISI
       showMedia();
@@ -92,12 +102,93 @@ const setLightbox = () => {
 
     // AFFICHE LE MEDIA CHOISI DANS LA LIGHTBOX
     const showMedia = () => {
-      // affiche titre media dans url
-      window.location.hash = selectedMedia.alt;
-      // affiche media + titre dans lightbox
-      lightboxMedia.src = selectedMedia.src;
-      lightboxMedia.alt = selectedMedia.alt;
-      lightboxTitle.textContent = selectedMedia.alt;
+      // // RESET LA LIGHTBOX
+      // // ----------- NE FONCTIONNE PAS ---------- //
+      // // Failed to execute 'removeChild' on 'Node': parameter 1 is not of type 'Node'
+      // const lightboxMedia = document.querySelectorAll('.lightbox__media');
+      // for (let i = 0; i < lightboxMedia.length; i++) {
+      //   if (lightboxMedia.length > 0) lightboxShow.removeChild(lightboxMedia);
+      // }
+
+      // SELON SON TYPE
+      // image
+      const imageType = selectMedia.querySelector('img.currentMedia');
+      console.log(imageType);
+      if (imageType) {
+        showImage();
+      }
+
+      // video
+      const videoType = selectMedia.querySelector('video.currentMedia');
+      console.log(videoType);
+      if (videoType) {
+        showVideo();
+        // showVideo() && lightboxShow.removeChild(img); // NE REMOVE PAS
+      }
+    };
+
+    // lightboxShow.removeChild(video); // BUG
+    // lightboxShow.removeChild(img); // BUG
+
+    const showImage = (image, title) => {
+      // RESET LA LIGHTBOX
+      // ----------- NE FONCTIONNE PAS ---------- //
+      // Failed to execute 'removeChild' on 'Node': parameter 1 is not of type 'Node'
+      const lightboxMedia = document.querySelectorAll('.lightbox__media');
+      console.log(lightboxMedia);
+      // lightboxShow = lightboxMedia.parentNode;
+      // console.log(lightboxShow); // ok
+
+      for (let i = 0; i < lightboxMedia.length; i++) {
+        console.log(lightboxShow); // ok // apparait quand length > 0
+        // if (lightboxMedia.length > 0) image.replaceWith(lightboxMedia);
+        // if (lightboxMedia.length > 0) lightboxMedia.replaceWith(image); // lightboxMedia.replaceWith is not a function
+        // if (lightboxMedia.length > 0) image.replaceWith(image); // Cannot read property 'replaceWith' of undefined
+        // if (lightboxMedia.length > 0) lightboxShow.removeChild(lightboxMedia);
+        // if (lightboxMedia.length > 0) lightboxShow.parentNode.removeChild(lightboxMedia);
+        // while (lightboxMedia.length > 0) image.replaceWith(lightboxMedia);
+        // while (lightboxMedia.length > 0) lightboxShow.removeChild(lightboxMedia);
+        // while (lightboxMedia.length > 0) lightboxShow.parentNode.removeChild(lightboxMedia);
+        // Failed to execute 'removeChild' on 'Node': parameter 1 is not of type 'Node'
+      }
+
+      // CREATION elmt IMAGE
+      image = document.createElement('img');
+      image.setAttribute('id', 'lightbox__img');
+      image.setAttribute('class', 'lightbox__media');
+      lightboxShow.appendChild(image);
+
+      // image.replaceWith(image) // NE FONCTIONNE PAS
+
+      // AFFICHE image
+      image.src = selectedMedia.src;
+      image.alt = selectedMedia.alt;
+
+      // CREATION elmt TITRE
+      title = document.createElement('p');
+      title.setAttribute('id', 'media__title');
+      title.setAttribute('class', 'lightbox__title');
+      lightboxShow.appendChild(title);
+      title.textContent = selectedMedia.alt;
+    };
+
+    const showVideo = (video, title) => {
+      // CREATION elmt VIDEO
+      video = document.createElement('video');
+      video.setAttribute('id', 'lightbox__video');
+      video.setAttribute('class', 'lightbox__media');
+      lightboxShow.appendChild(video);
+
+      // AFFICHE video
+      video.src = selectedMedia.src;
+      video.alt = selectedMedia.alt;
+
+      // CREATION elmt TITRE
+      title = document.createElement('p');
+      title.setAttribute('id', 'media__title');
+      title.setAttribute('class', 'lightbox__title');
+      lightboxShow.appendChild(title);
+      title.textContent = selectedMedia.alt;
     };
   }
 };

e3da3e42cc0cb740a6387bc6ec314d44730d4f67
Amandine Arias <amandine.arias@gmail.com>
2021-06-04 11:43:54 +0200

    FIX (index) : multiple tags filtrering (#2)

--- js/home_page.js
+++ js/home_page.js
@@ -1,127 +1,93 @@
 let url =
   'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json';
 
-function fetchData() {
-  // APPEL API, CALLBACK, (...) RETOUR PROMESSE
-  fetch(url)
-    .then((response) => response.json())
-    // RECUPERATION DES DONNEES
-    .then((data) => {
-      console.log(data);
+// APPEL API, CALLBACK, (...) RETOUR PROMESSE
+fetch(url)
+  .then((response) => response.json())
+  // RECUPERATION DES DONNEES
+  .then((data) => {
+    // console.log(data);
 
-      // AFFICHE PHOTOGRAPHES SELON TAG CHOISI DANS SA CARTE
-      // RECUPERE LE TAG CHOISI via url
-      const getTag = (urlTag, tag) => {
-        urlTag = new URL(document.location).searchParams;
-        tag = urlTag.get('tag');
-        return tag;
-      };
-      // console.log(getTag());
+    // AFFICHE PHOTOGRAPHES SELON TAG CHOISI DANS SA CARTE
+    // RECUPERE LE TAG CHOISI via url
+    const getTag = (urlTag, tag) => {
+      urlTag = new URL(document.location).searchParams;
+      tag = urlTag.get('tag');
+      return tag;
+    };
+    // console.log(getTag());
 
-      // CIBLE LES PHOTOGRAPHES EN FONCTION DU TAG CHOISI
-      for (let i = 0; i < data.photographers.length; i++) {
-        let selectedTag = data.photographers[i].tags.filter(
-          (elmt) => elmt == getTag()
-        );
-        // console.log(data.photographers[i].tags);
-        // console.log(selectedTag);
+    // CIBLE LES PHOTOGRAPHES EN FONCTION DU TAG CHOISI
+    for (let i = 0; i < data.photographers.length; i++) {
+      let selectedTag = data.photographers[i].tags.filter(
+        (elmt) => elmt == getTag()
+      );
 
-        // AFFICHE LA CARTE DE TOUS LES PHOTOGRAPHES QUAND AUCUN TAG CHOISI
-        if (getTag() == null) {
-          setPhotographers(data.photographers[i]);
-        } else {
-          // AFFICHE LA CARTE DES PHOTOGRAPHES CORRESPONDANT AU TAG CHOISI
-          for (let j = 0; j < selectedTag.length; j++) {
-            if (selectedTag[j].length > 0) {
-              setPhotographers(data.photographers[i]);
-            }
+      // AFFICHE LA CARTE DE TOUS LES PHOTOGRAPHES QUAND AUCUN TAG CHOISI
+      if (getTag() == null) {
+        setPhotographers(data.photographers[i]);
+      } else {
+        // AFFICHE LA CARTE DES PHOTOGRAPHES CORRESPONDANT AU TAG CHOISI
+        for (let j = 0; j < selectedTag.length; j++) {
+          if (selectedTag[j].length > 0) {
+            setPhotographers(data.photographers[i]);
           }
         }
       }
+    }
 
-      // AFFICHE PHOTOGRAPHES SELON TAG(S) CHOISI(S) DANS LA BARRE DE NAVIGATION
-      // ---------------------------------------------------------------------- //
-      // SI 2 TAGS CORRESPONDENT AU MEME PHOTOGRAPHE > NE L'AFFICHER QU'1 FOIS
-      // ---------------------------------------------------------------------- //
+    // DOM elements
+    const photographersSection =
+      document.getElementsByClassName('photographers')[0];
+    const tags = document.querySelectorAll('.tagNav a');
+    for (let i = 0; i < tags.length; i++) {
+      // quand click sur tag
+      tags[i].addEventListener('click', (event) => {
+        event.preventDefault();
 
-      // DOM elements
-      const photographersSection =
-        document.getElementsByClassName('photographers')[0];
-      const tags = document.querySelectorAll('.tagNav a');
-      for (let i = 0; i < tags.length; i++) {
-        // quand click sur tag
-        tags[i].addEventListener('click', (event) => {
-          event.preventDefault();
+        // SUPPRIME LES CARTES DEJA CHARGEES
+        photographersSection.innerHTML = '';
 
-          // SUPPRIME LES CARTES DEJA CHARGEES
-          photographersSection.innerHTML = '';
+        // ajoute toggle "selected"
+        tags[i].classList.toggle('selected');
+        const selected = document.querySelectorAll('a.selected');
 
-          // ajoute toggle "selected"
-          tags[i].classList.toggle('selected');
-          const selected = document.querySelectorAll('a.selected');
-
-          // tableau des tags choisis
-          let tagsList = Array.from(selected).map((elmt) => {
-            elmt = elmt.title;
-            return elmt;
-          });
-          console.log(tagsList);
-
-          // recupere l'id de chaque photographe
-          var photographerId =
-            document.getElementsByClassName('photographerId');
-
-          // pour chaque tag du tableau
-          tagsList.filter((tag) => {
-            // affiche photographe(s) dont tag(s) correspond(ent)
-            for (let j = 0; j < data.photographers.length; j++) {
-              // liste des tags de chaque photographe
-              // console.log(data.photographers[j].tags);
-
-              const selectedTags = data.photographers[j].tags.some(
-                (elmt) => elmt == tag
-              );
-              // console.log(selectedTags);
-
-
-              // si tag(s) choisi(s) affiche photographes correspondants
-              if (selectedTags)
-
-              // ICI IL FAUDRAIT RECUPER LE DERNIER RESULTAT DE LA FONCTION
-              // uniqueId SITUEE EN DEHORS DE CETTE BOUCLE
-              // J'AI ESSAYE LA METHODE ASYNC / AWAIT
-              // MAIS NE PARVIENS A AUCUN RESULTAT CONCLUANT...
-
-              setPhotographers(data.photographers[j]);
-              
-              // ou
-              //   const filterPage = async () => {
-              //     if (selectedTags) setPhotographers(data.photographers[j]);
-              //   };
-              //   filterPage();
-            }
-
-            // FONCTION : RETIRE DOUBLONS ID PHOTOGRAPHES dont plusieurs tags correspondent
-            const uniqueId = (idList) => {
-              idList = Array.from(photographerId).map((id) => {
-                id = id.id;
-                return id;
-              });
-              return [...new Set(idList)];
-            };
-            console.log(uniqueId());
-          });
-
-          // AFFICHE TOUS LES PHOTOGRAPHES SI AUCUN TAG N'EST CHOISI
-          if (tagsList.length == 0) location.reload();
+        // tableau des tags choisis
+        let tagsList = Array.from(selected).map((elmt) => {
+          elmt = elmt.title;
+          return elmt;
         });
-      }
-    });
+        // console.log(tagsList);
+
+        // tableau vide pour recuperation tags choisi
+        let photographers = [];
+
+        // pour chaque tag choisi
+        tagsList.filter((tag) => {
+          for (let j = 0; j < data.photographers.length; j++) {
+            const selectedTags = data.photographers[j].tags.some(
+              (elmt) => elmt == tag
+            );
+            // console.log(selectedTags); // correspondance tag true / false
+
+            // si tag(s) choisi(s), recupere photographes correspondants dans tableau dedie
+            if (selectedTags && !photographers.includes(data.photographers[j]))
+              photographers.push(data.photographers[j]);
+          }
+        });
+        // affiche photographe(s) dont tag(s) correspond(ent)
+        photographers.forEach((photographer) => {
+          setPhotographers(photographer);
+        });
+
+        // AFFICHE TOUS LES PHOTOGRAPHES SI AUCUN TAG N'EST CHOISI
+        if (tagsList.length == 0) location.reload();
+      });
+    }
+  })
 
   // GESTION DES ERREURS
-  // .catch((error) => console.log(error.message));
-}
-fetchData();
+  .catch((error) => console.log(error.message));
 
 // GESTION DU BOUTON D'EVITEMENT SELON SCROLL
 // mobile screens

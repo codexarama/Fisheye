9322ab510e401e22027e1d81986f783bd482f0f2
Amandine Arias <amandine.arias@gmail.com>
2022-06-08 14:51:21 +0200

    refactor (handle_focus)

--- js/handle_focus.js
+++ js/handle_focus.js
@@ -1,16 +1,16 @@
 // ACCEDE AUX STYLES
 var head = document.head || document.getElementsByTagName('head')[0];
-var axsStyles = head.appendChild(document.createElement('style'));
+var accessStyles = head.appendChild(document.createElement('style'));
 
 // SI NAVIGATION SOURIS
 // ANNULE LES STYLES AFFECTES AU FOCUS EN NAVIGATION CLAVIER
 document.addEventListener('mousedown', () => {
-  axsStyles.innerHTML = '* {box-shadow:none !important}';
+  accessStyles.innerHTML = '* {box-shadow:none !important}';
 });
 
 // SI NAVIGATION CLAVIER
 // ANNULE LES STYLES PAR DEFAUT DU FOCUS
 document.addEventListener('keydown', () => {
-  axsStyles.innerHTML =
+  accessStyles.innerHTML =
     '*:not(.gallery__link):not(.lightbox__title) {outline:none !important}';
 });
